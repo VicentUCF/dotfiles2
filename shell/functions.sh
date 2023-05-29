1880b27e87c78400220d3ca846a93c5b69451a49
VicentUCF <vicentciscar17@gmail.com>
2023-05-29 20:36:08 +0200

    feat" add export and load apps

--- shell/functions.sh
+++ shell/functions.sh
@@ -17,3 +17,16 @@ function recent_dirs() {
 
 	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
 }
+
+function export_apps(){
+  npm list -g --depth 0 | grep -v npm >"$DOTFILES_PATH/langs/js/global_modules.txt"
+  volta list all --format plain | awk '{print $2}' >"$DOTFILES_PATH/langs/js/global_modules.txt"
+  pacman -Qm | awk '{print $1}' >"$DOTFILES_PATH/os/arch/yay/global_packages.txt"
+  comm -23 <(pacman -Qqt | sort) <({ pacman -Qqg xorg; echo base; } | sort -u) >"$DOTFILES_PATH/os/arch/pacman/global_packages.txt"
+}
+
+function import_apps(){
+  cat "$DOTFILES_PATH/langs/js/global_modules.txt" | xargs -I % volta install %
+  cat "$DOTFILES_PATH/os/arch/yay/global_packages.txt" | xargs -I % yay -S %
+  cat "$DOTFILES_PATH/os/arch/pacman/global_packages.txt" | xargs -I % sudo pacman -S --needed %
+}
