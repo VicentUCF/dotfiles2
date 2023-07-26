0b45e0777a6b09a421369d6dfd00fe1850525453
Vicent Ciscar <vicentciscar17@gmail.com>
2023-07-26 17:17:18 +0200

    fix: all dotfiles

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,2 +1,3 @@
 zimfw() { source /home/vicent_ucf/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
 zmodule() { source /home/vicent_ucf/.dotfiles/shell/zsh/.zim/zimfw.zsh "${@}" }
+source /home/vicent_ucf/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
